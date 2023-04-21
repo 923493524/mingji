a3171497e53117f14e44414a5ef9043e1f0656a8
923493524 <923493524@qq.com>
2023-04-21 17:56:53 +0800

    init

---
+++ src/main/java/com/lmq/weixinminiproject/config/HttpConverterConfig.java
@@ -0,0 +1,31 @@
+package com.lmq.weixinminiproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class HttpConverterConfig implements WebMvcConfigurer {
+
+    @Configuration
+    public class WebMVCConfig extends WebMvcConfigurationSupport {
+
+        // 路径映射配置
+        @Override
+        protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+
+            registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/");
+            // 磁盘目录
+            String path = "D:/ruoyi/uploadPath/";
+            // 将网页地址栏中的 /image/**  映射到  磁盘的某个目录下，以访问磁盘资源
+            registry.addResourceHandler("/profile/**").addResourceLocations("file:" + path);
+
+        }
+
+    }
+
+
+}
+
